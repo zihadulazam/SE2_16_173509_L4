021a30ce81235808b36c09e53719e8adb06734c8
Azam Zihadul (ACC0084780) <zihadul.azam@sci-a202pc28x.unitn.it>
2016-11-22 11:33:59 +0100

    aggiunto commenti

--- moduli/datamanager.js
+++ moduli/datamanager.js
@@ -1,6 +1,14 @@
 var employees=[];
 var nextID=3;
 
+/**
+ * @brief Oggetto employee
+ * @param [in] int id id del employee.
+ * @param [in] string name nome del employee.
+ * @param [in] string surname cognome del employee.
+ * @param [in] int level livello del contratto.
+ * @param [in] int salary stipendio del employee.
+ */
 function employee(id,name,surname,level,salary) {
     this.id=id;
     this.name=name;
@@ -9,12 +17,23 @@ function employee(id,name,surname,level,salary) {
     this.salary=salary;
 }
 
+/**
+ * @brief inizializza employe inserendo tre employee di default
+ */
 function initEmp() {
     employees.push(new employee(0,"Mario","Rossi",5,2000));
     employees.push(new employee(1,"Marco","Stasi",2,1000));
     employees.push(new employee(2,"Roberto","Trentini",5,3000));
 }
 
+/**
+ * @brief inderisce un employee
+ * @param [in] int id id del employee.
+ * @param [in] string name nome del employee.
+ * @param [in] string surname cognome del employee.
+ * @param [in] int level livello del contratto.
+ * @param [in] int salary stipendio del employee.
+ */
 function insert(id,name,surname,level,salary){  //ok insert
     if((id=='' || verifyInt(id)) && verifyInt(level) && verifyInt(salary)){
         //verifico se è senza id
@@ -55,7 +74,11 @@ function insert(id,name,surname,level,salary){  //ok insert
     }
 }
 
-
+/**
+ * @brief cerca un employee con id
+ * @param [in] int id id del employee.
+ * @return ritorna l'indice dove si trova nel array
+ */
 function find(id) {
     var i;
     for(i=0;i<employees.length;i++){
@@ -65,11 +88,19 @@ function find(id) {
     return -1;
 }
 
+/**
+ * @brief elimina un employee con id
+ * @param [in] int id id del employee.
+ */
 function deleteElem(ind) {
     employees.splice(ind,1);
 }
 
-
+/**
+ * @brief ritorna un oggetto employee con id
+ * @param [in] int index indice del array dove si trova.
+ * @return ritorna l'oggetto employee
+ */
 function getEmployeeByIndex(index) {
     if(index>=0 && index<employees.length){
         return employees[index];
@@ -78,11 +109,16 @@ function getEmployeeByIndex(index) {
         return null;
 }
 
+/**
+ * @brief verifica se un var è maggiore di 0 
+ * @return ritorna true se si se no ritorna false 
+ */
 function verifyInt(a) {
     var res;
     res=a>=0? true: false;
     return res;
 }
+
 //export functions
 exports.initEmp = initEmp;
 exports.getEmployeeByIndex = getEmployeeByIndex;
